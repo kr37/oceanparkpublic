03f84f4e203f21479b5ec163adb625379fc633ef
Kelsang Rinzin <kelsangrinzin@gmail.com>
2021-06-08 15:01:02 -0500

    Admin now shows attendees. Other cleanup.
    	modified:   admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -16,8 +16,10 @@
     include_once("../../../nonpublic/oceanpark/opBase.php");
     include_once(NONPUBLIC_OP . "opDatabase.php");
 
-    function table($array, $headers=[]) {
+    function table($title, $array, $headers=[]) {
+        ksort($array, SORT_NATURAL); 
         $s = '         ';
+        echo "      <h2>$title</h2>\n";
         echo "      <table>\n";
         if (count($headers)) {
             echo "$s<thead>\n";
@@ -43,17 +45,23 @@
 TABLE;
     }
 
-    echo "<h2>Registrants</h2>\n";
     foreach ($registrants as &$registrant) unset($registrant['totalCost'], $registrant['stillToPay']);
-    table ($registrants, ['Email', 'Name', 'Phone', 'Center', 'Transactions', 'Carts']);
+    table ('Registrants',
+            $registrants, 
+            ['Email', 'Name', 'Phone', 'Center', 'Transactions', 'Carts']);
 
-    echo "<h2>Transactions</h2>\n";
-    ksort($transactions, SORT_NATURAL); 
-    table($transactions, ['Registrant email', 'Email at PayPal', 'Cart ID', 'txn_id', 'Paid', 'Cart Total', 'All PayPal fields']);
+    table('Transactions',
+            $transactions, 
+            ['Registrant email', 'Email at PayPal', 'Cart ID', 'txn_id', 'Paid', 'Cart Total', 'All PayPal fields']);
 
-    echo "<h2>Carts</h2>\n";
-    ksort($carts, SORT_NATURAL);
-    table($carts, ['Creation time', 'Registrant email', 'Total', 'Paid', 'Due', 'Transaction IDs', 'All the cart data']);
+    table('Carts',
+            $carts, 
+            ['Creation time', 'Registrant email', 'Total', 'Paid', 'Due', 'Transaction IDs', 'All the cart data']);
+
+    foreach ($attendees as &$att) unset($att['isChild'], $att['carers']);
+    table('Attendees',
+            $attendees, 
+            ['Attendee name', 'Registrant email', 'Room ID', 'Linens (1=yes)', 'Cost', 'Address', 'Emergency contact', 'Food', 'Medical']);
     ?>
 
 </body>
